6f9a0446f214ab54bbe1168b536ccaae46b07fcc
jlebre <josemariamlebre@gmail.com>
2022-09-16 00:55:07 +0100

    Update

--- wrong_map_tester.sh
+++ wrong_map_tester.sh
@@ -9,41 +9,41 @@ echo "\nTest 2 - Empty Parameter!\n"
 ./so_long ""
 sleep 0.5
 echo "\nTest 3 - Empty Map!\n"
-./so_long maps/maps_that_dont_work/map_empty.ber
+./so_long maps_that_dont_work/map_empty.ber
 sleep 0.5
 echo "\nTest 4 - Wrong name (.bera)!\n"
-./so_long maps/maps_that_dont_work/map_with_wrong_name.bera
+./so_long maps_that_dont_work/map_with_wrong_name.bera
 sleep 0.5
 echo "\nTest 5 - Map without .ber!\n"
-./so_long maps/maps_that_dont_work/map_without_ber
+./so_long maps_that_dont_work/map_without_ber
 sleep 0.5
 echo "\nTest 6 - Map is not Rectangular!\n"
-./so_long maps/maps_that_dont_work/map_not_rectangular.ber
+./so_long maps_that_dont_work/map_not_rectangular.ber
 sleep 0.5
 echo "\nTest 7 - Error on Bottom Wall!\n"
-./so_long maps/maps_that_dont_work/map_with_no_bottom_wall.ber
+./so_long maps_that_dont_work/map_with_no_bottom_wall.ber
 sleep 0.5
 echo "\nTest 8 - Error on Side Wall!\n"
-./so_long maps/maps_that_dont_work/map_with_no_side_wall.ber
+./so_long maps_that_dont_work/map_with_no_side_wall.ber
 sleep 0.5
 echo "\nTest 9 - Error on Top Wall!\n"
-./so_long maps/maps_that_dont_work/map_with_no_top_wall.ber
+./so_long maps_that_dont_work/map_with_no_top_wall.ber
 sleep 0.5
 echo "\nTest 10 - Map with unrecognized character!\n"
-./so_long maps/maps_that_dont_work/map_with_unrecognized_character.ber
+./so_long maps_that_dont_work/map_with_unrecognized_character.ber
 sleep 0.5
 echo "\nTest 11 - Map with two players!\n"
-./so_long maps/maps_that_dont_work/map_with_two_players.ber
+./so_long maps_that_dont_work/map_with_two_players.ber
 sleep 0.5
 echo "\nTest 12 - Map without players!\n"
-./so_long maps/maps_that_dont_work/map_without_players.ber
+./so_long maps_that_dont_work/map_without_players.ber
 sleep 0.5
 echo "\nTest 13 - Map with two exits!\n"
-./so_long maps/maps_that_dont_work/map_with_two_exits.ber
+./so_long maps_that_dont_work/map_with_two_exits.ber
 sleep 0.5
 echo "\nTest 14 - Map without exits!\n"
-./so_long maps/maps_that_dont_work/map_without_exits.ber
+./so_long maps_that_dont_work/map_without_exits.ber
 sleep 0.5
 echo "\nTest 15 - Map without valid path!\n"
-./so_long maps/maps_that_dont_work/map_without_valid_path.ber
+./so_long maps_that_dont_work/map_without_valid_path.ber
 sleep 0.5
